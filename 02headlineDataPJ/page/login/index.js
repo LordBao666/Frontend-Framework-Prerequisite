be91ff226ded52e71f60c51c12669e614d67798e
LordBao666 <916900021@qq.com>
2024-08-21 16:19:48 +0800

    Finish login by Verification Code

--- 02headlineDataPJ/page/login/index.js
+++ 02headlineDataPJ/page/login/index.js
@@ -5,3 +5,38 @@
  * 1.3 基于 axios 调用验证码登录接口
  * 1.4 使用 Bootstrap 的 Alert 警告框反馈结果给用户
  */
+
+/**
+ * @param {boolean} isLoginSuccess if login is successful
+ * @param {string} message :the message to be shown on the screen
+ */
+function loginFeedback(isLoginSuccess,message){
+  const alertDiv=document.querySelector(".alert");
+  alertDiv.classList.add("show");
+  let alerStr=isLoginSuccess?"alert-success":"alert-danger";
+  alertDiv.classList.add(alerStr);
+  alertDiv.textContent=message;
+
+  //after 2 secs, alert div disappears
+  setTimeout(()=>{
+    alertDiv.classList.remove("show");
+    alertDiv.classList.remove(alerStr);
+  },2000);
+}
+
+const loginButton = document.querySelector(".login-form .btn");
+loginButton.addEventListener("click", async (event) => {
+  try {
+    const form = document.querySelector(".login-form");
+    const data = serialize(form, { hash: true, empty: true });
+    const result = await axios({
+      url: "/v1_0/authorizations",
+      method:"post",
+      data
+    });
+    loginFeedback(true,result.data.message);
+
+  } catch (error) {
+    loginFeedback(false,error.response.data.message);
+  }
+});
